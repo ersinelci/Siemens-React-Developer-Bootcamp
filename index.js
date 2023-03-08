43d99f80c74e35ba2c55fa5391d6c13815b476f2
ersinelci <ersinelci@gmail.com>
2023-03-08 10:45:49 +0300

    first commit - git branch'ı kontrol ediliyor.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Merhaba");
+var slugify = require('slugify');
+var result = slugify("BUgün İstanbul için güneşli bir hava bekleniyor.", {
+    lower:true,
+    replacement: '*'
+});
+
+console.log(result);
